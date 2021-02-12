5e7247a1b06e9c503d2674b6d8a459e0e0d98449
Rameshaaa <rammech465@gmail.com>
2021-02-12 12:02:42 +0530

    new commit 2/12/2021 - change the packages from src/test to src/main

--- src/main/java/com/mas/resources/Base.java
+++ src/main/java/com/mas/resources/Base.java
@@ -22,9 +22,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
-
 public class Base {
 	
 	public WebDriver driver;
